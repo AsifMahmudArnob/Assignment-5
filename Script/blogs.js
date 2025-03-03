9820c71568b045af470d9457743fd7651380b843
AsifMahmudArnob <floppymythic@gmail.com>
2025-03-03 19:23:11 +0600

    fifth commit

--- Script/blogs.js
+++ Script/blogs.js
@@ -1,12 +1,15 @@
 function myFunction() {
-    location.replace("http://127.0.0.1:5500/blogs.html")
-  }
-function blogFunction(){
-    location.replace("http://127.0.0.1:5500/index.html")
+  window.location.href = "blogs.html";
+
+}
+
+function blogFunction() {
+  window.location.href = "index.html";
+
 }
 // Theme Color
 
-const colors=['gray','red','blue','green','yellow','violet','cyan','brown','white','black'];
+const colors=['#97E7E1','#FFC96F','#C4D7FF','#FFDFEF','#B7B1F2','#6A9C89','#B4EBE6','#B6FFA1','white','#FBA1B7'];
 let colorIndex = 0;
 
 document.getElementById("themeColor").addEventListener("click",function(){
